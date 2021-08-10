abfe9c3e664bdc7e71357c064b5b17e431537d85
Lin Hong <triaton00@gmail.com>
2021-08-10 14:28:18 +0900

    chore: refator wagyu to pepe

--- lib/provider.ts
+++ lib/provider.ts
@@ -4,7 +4,7 @@ import { HttpProvider } from "web3-core";
 let provider: HttpProvider | undefined;
 export const getProvider = () => {
   if (!provider || (provider && !provider.connected)) {
-    provider = new Web3.providers.HttpProvider("https://explorer.testnet.velas.com/rpc");
+    provider = new Web3.providers.HttpProvider("https://data-seed-prebsc-1-s1.binance.org:8545");
   }
   return provider;
 };
